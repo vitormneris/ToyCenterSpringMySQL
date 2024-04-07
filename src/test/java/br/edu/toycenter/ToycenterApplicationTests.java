843fbf026e18f4224b00148325b1bfaf31d07810
João Vitor Moreira dos Santos Neris <joaomoreiraneris0@gmail.com>
2024-04-06 21:55:20 -0300

    Project created

---
+++ src/test/java/br/edu/toycenter/ToycenterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.toycenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToycenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
