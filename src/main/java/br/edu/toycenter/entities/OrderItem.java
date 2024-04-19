cbfb7327518521b12f8d2c7e0015c23b67e08fd0
João Vitor Moreira dos Santos Neris <joaomoreiraneris0@gmail.com>
2024-04-18 22:46:09 -0300

    OrderItem, many-to-many association with extra attributes PART 1

---
+++ src/main/java/br/edu/toycenter/entities/OrderItem.java
@@ -0,0 +1,82 @@
+package br.edu.toycenter.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import br.edu.toycenter.entities.pk.OrderItemPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private OrderItemPK id;
+	
+	private int quantity;
+	private Double price;
+	
+	public OrderItem() {
+		
+	}
+
+	public OrderItem(Product product, Order order, int quantity, Double price) {
+		super();
+		id.setProduct(product);
+		id.setOrder(order);
+		this.quantity = quantity;
+		this.price = price;
+	}
+
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void setProduct(Product product) {
+		id.setProduct(product);
+	}
+	
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
+	}
+}
