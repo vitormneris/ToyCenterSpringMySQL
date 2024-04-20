05a07918ff4c9cfc9c0825fe3889a594a17a3a23
João Vitor Moreira dos Santos Neris <joaomoreiraneris0@gmail.com>
2024-04-20 19:09:49 -0300

    Subtotal & Total methods

--- src/main/java/br/edu/toycenter/entities/Order.java
+++ src/main/java/br/edu/toycenter/entities/Order.java
@@ -101,6 +101,14 @@ public class Order implements Serializable {
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
+	
+	public Double getTotal() {
+		Double sum = 0.0;
+		for (OrderItem item : getItems()) {
+			sum += item.getSubTotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
