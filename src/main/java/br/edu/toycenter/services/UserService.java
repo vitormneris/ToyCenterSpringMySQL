729f3989bf8caad97c07dd5eb52e9c468cf18533
João Vitor Moreira dos Santos Neris <joaomoreiraneris0@gmail.com>
2024-04-20 22:22:41 -0300

    Exception handling - findById

--- src/main/java/br/edu/toycenter/services/UserService.java
+++ src/main/java/br/edu/toycenter/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import br.edu.toycenter.entities.User;
 import br.edu.toycenter.repositories.UserRepository;
+import br.edu.toycenter.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User user) {
